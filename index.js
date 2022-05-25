dd888d07f63109ae27c462ae7c63e333d78298c1
chetan <chtnbarod@gmail.com>
2022-05-25 11:38:02 +0530

    ok

--- index.js
+++ index.js
@@ -3,27 +3,24 @@ const app = express();
 const router = express.Router();
 
 
+
 // Configuring body parser middleware
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-  // view router
-  app.use('/', router);
+// view router
+app.use('/', router);
 
-  router.get('/',function(req,res){
-      res.sendFile("public/home.html" , { root : __dirname });
+router.get('/',function(req,res){
+    res.sendFile("public/home.html" , { root : __dirname });
 
-    });
-
-
-  // api router
-  router.post('/test', (req, res) => {
-    return `${JSON.stringify(req.body)}`;
   });
 
+router.post('/test', (req, res) => {
+  res.send(`${JSON.stringify(req.body)}`);
+});
 
-// Running server
 const port = process.env.port || 3001;
 app.listen(port);
-console.log(`http://localhost:${port}`);
+console.log(`Running at  http://localhost:${port}`);
