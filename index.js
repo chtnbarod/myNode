61bef0c2b9d92a0029cfc86108d7246830487a53
chetan <chtnbarod@gmail.com>
2022-05-25 11:23:58 +0530

    ok

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const router = express.Router();
-const port = process.env.port || 3001;
-
-
-
-// view router
-app.use('/', router);
 
 
 // Configuring body parser middleware
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.get('/',function(req,res){
-    res.sendFile("public/home.html" , { root : __dirname });
+  // view router
+  app.use('/', router);
 
+  router.get('/',function(req,res){
+      res.sendFile("public/home.html" , { root : __dirname });
+
+    });
+
+
+  // api router
+  router.post('/test', (req, res) => {
+    return `${JSON.stringify(req.body)}`;
   });
 
-app.post('/test', (req, res) => {
-  return `${JSON.stringify(req.body)}`;
-});
 
+// Running server
+const port = process.env.port || 3001;
 app.listen(port);
-console.log(`Running at  http://localhost:${port}`);
+console.log(`http://localhost:${port}`);
